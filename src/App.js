3d0b9ab1a520de7321eaf8af7e10fe2c80d53f66
Diego <diego.silva@profite.com.br>
2018-01-26 00:03:54 -0200

    Start construct header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import Header from './components/header/header.js';
-import logo from './logo.svg';
+import Header from './Components/Header/Header.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
       </div>
     );
   }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
+import logo from './img/Logo.png';
 import './header.css';
 
 class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log('Click happened');
+  }
+
   render() {
     return (
-      <div className="header">
-        <div className="Myimg">
+        <div className="header">
+          <div className="header__nav">
+              <div className="header__nav-icon" onClick={this.handleClick}>
+                <span className="header__nav-icon"></span>
+                <span className="header__nav-icon"></span>
+                <span className="header__nav-icon"></span>
+              </div>
+              <div className="header__nav-body">
+                  <ul>
+                    <li><a href="index.html">Eu</a></li>
+                    <li><a href="portfolio.html">Portfólio</a></li>
+                    <li><a href="conheca.html">Let’s GO Learn</a></li>
+                  </ul>
+              </div>
+          </div>
+          <div className="header__img">
           <a href="/index.html">
-              <img src="src/images/Logo.png" alt="Logo DieGO" />
+              <img src={logo} alt="Logo DieGO" />
           </a>
-        </div>
-        <div className="nav_linha">
-            <div className="nav">
-                <ul>
-                  <li><a href="index.html">Eu</a></li>
-                  <li><a href="portfolio.html">Portfólio</a></li>
-                  <li><a href="conheca.html">Let’s GO Learn</a></li>
-                </ul>
-            </div>
         </div>
       </div>
     );
